8b3003689005cd7c33c04392ec46b10b0f6e34f8
Aniket Mane <aniket.mane84@gmail.com>
2021-12-23 00:49:49 +0530

    accept input and forward to spring boot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useEffect,useState} from 'react';
-import { Container } from 'reactstrap';
+import { Row,Container,Col,Input,Button,InputGroup} from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import Axios from 'axios';
@@ -7,19 +7,34 @@ import Axios from 'axios';
 
 const App=()=> {
   const [response, setResponse] = useState("");
-  const fetchResponse=async ()=>{
-    const res=await Axios.get('http://localhost:8080/home');
+  const [query,setQuery]=useState('');
+
+  const fetchDetails=async ()=>{
+    const res=await Axios.get(`http://localhost:8080/home/${query}`);
     console.log(res);
     setResponse(res.data);
   }
 
-  useEffect(() => {
-    fetchResponse();
-  }, [])
+  // useEffect(() => {
+  //   fetchResponse();
+  // }, []);
   return (
-    <div className="App">
-      <span>{response}</span>
-    </div>
+    <Container>
+      <Row className=" mt-3">
+        <Col md="5">
+          <InputGroup>
+            <Input
+              type="text"
+              value={query}
+              onChange={e=> setQuery(e.target.value)}
+              placeholder="Please provide the username"
+            />
+              <Button onClick={fetchDetails} color="primary">Fetch Data</Button>
+          </InputGroup>
+        </Col>
+        <Col md="7">{response}</Col>
+      </Row>
+    </Container>
   );
 }
 
